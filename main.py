cad79d89d00376984ede0f1546e9157ca4aea8cf
Спешилов Александр Андреевич <SpeshilovAA@npoa.org>
2022-08-24 09:30:03 +0500

    added frame_difference()

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import datetime
 import time
+import sys
 
 
-PATH_OLD = '.\\archive of notifications DSO\\notifications DSO 2022-05-26.xlsx'
+PATH_OLD = '.\\archive of notifications DSO\\notifications DSO 2022-07-29.xlsx'
 PATH_NEW = '.\\central archive OTD\\notifications DSO.xlsx'
 PATH_DIFF = '.\\new receipts of DSO'
 
-def frame_to_dict(frame, keys: str):
+
+def frame_to_dict(frame: pd.DataFrame, keys: str):
+    """Convert frame to dictionary of dictionaries by keys."""
     dict_of_dict = dict()
     for i in range(len(frame.index)):
         key = frame[keys][i]
@@ -17,18 +20,46 @@ def frame_to_dict(frame, keys: str):
                 dict_of_dict[key][column] = frame[column][i]
     return dict_of_dict
 
+def frame_difference(frame1: pd.DataFrame, frame2: dict, keys: str):
+    """Return frame1 - frame2."""
+    data_diff = pd.DataFrame(columns=frame1.columns)
+    for i in range(len(frame1.index)):
+        key = frame1[keys][i]
+        
+        # New documentation.
+        if key not in frame2:
+            data_diff = pd.concat([data_diff,
+                                   pd.DataFrame(frame1.loc[i]).T],
+                                  ignore_index=True)
+            continue
+
+        # Changed documentation.
+        else:
+            if frame1['№  изм.'][i] != frame2[key]['№  изм.']:
+                data_diff = pd.concat([data_diff,
+                                       pd.DataFrame(frame1.loc[i]).T],
+                                      ignore_index=True)
+
+    return data_diff
+
 def main():
 
     time_start = time.time()
 
-##    data_old = pd.read_excel(PATH_OLD, skiprows=[0])
-##    data_new = pd.read_excel(PATH_NEW, skiprows=[0])
-##
+    data_old = pd.read_excel(PATH_OLD, skiprows=[0])
+    data_new = pd.read_excel(PATH_NEW, skiprows=[0])
+    
+    data_diff = frame_difference(
+        data_new, frame_to_dict(data_old, 'Обозначение документа'),
+        keys='Обозначение документа')
+
+##    print(data_diff, end='\n\n')
+
 ##    data_diff = pd.DataFrame(
 ##        columns=['Обозначение документа', '№  изм.', 'Наименование', 'Город',
 ##                 'Организация', 'Дата ввода информации (фильтр новых поступлений)',
 ##                 'Примечание'])
-##
+
 ##    for i in range(len(data_new.index)):
 ##        match = False  # Совпадение с записью из старого списка.
 ##        for j in range(len(data_old.index)):
@@ -44,27 +75,28 @@ def main():
 ##        if not match:
 ##            data_diff = pd.concat([data_diff, pd.DataFrame(data_new.loc[i]).T],
 ##                                  ignore_index=True)
+
+    today = datetime.date.today()
+    name_diff = PATH_DIFF + '\\new receipts of DSO ' + str(today) + '.xlsx'
+
+    writer = pd.ExcelWriter(name_diff)
+    data_diff.to_excel(writer, sheet_name=str(today), index=False)
+    # Auto-adjust columns' width
+    for column in data_diff:
+        column_width = max(data_diff[column].astype(str).map(len).max(), len(column))
+        col_idx = data_diff.columns.get_loc(column)
+        writer.sheets[str(today)].set_column(col_idx, col_idx, column_width)
+    writer.save()
+
+
+
+##    data_test = pd.read_excel('test_file.xlsx')
+##    print(data_test, end='\n\n')
+##    print('size of data test', sys.getsizeof(data_test), end='\n\n')
 ##
-##    today = datetime.date.today()
-##    name_diff = PATH_DIFF + '\\new receipts of DSO ' + str(today) + '.xlsx'
-
-##    writer = pd.ExcelWriter(name_diff)
-##    data_diff.to_excel(writer, sheet_name=str(today), index=False)
-##    # Auto-adjust columns' width
-##    for column in data_diff:
-##        column_width = max(data_diff[column].astype(str).map(len).max(), len(column))
-##        col_idx = data_diff.columns.get_loc(column)
-##        writer.sheets[str(today)].set_column(col_idx, col_idx, column_width)
-##    writer.save()
-
-
-    data_test = pd.read_excel('test_file.xlsx')
-    print(data_test, end='\n\n')
-    
-    dict_test = frame_to_dict(data_test, 'Обозначение')
-    for key, value in dict_test.items():
-        print(key, value, sep=':\t')
-
+##    dict_test = frame_to_dict(data_test, 'Обозначение')
+##    print(dict_test, end='\n\n')
+##    print('size of dict test', sys.getsizeof(dict_test), end='\n\n')
 
 
     time_finish = time.time()
