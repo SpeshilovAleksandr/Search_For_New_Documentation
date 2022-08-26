c0d60cca453dfa0f7b6ac3e9f28aeeb768c43096
Спешилов Александр Андреевич <SpeshilovAA@npoa.org>
2022-08-26 14:22:43 +0500

    added find_identical_cells() - and merging cells

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import datetime
 import pandas as pd
 
 
-##PATH_OLD = '.\\archive of notifications DSO\\notifications DSO 2022-07-29.xlsx'
-##PATH_NEW = '.\\central archive OTD\\notifications DSO.xlsx'
-PATH_OLD = '.\\archive of notifications DSO\\test_file_old.xlsx'
-PATH_NEW = '.\\central archive OTD\\test_file_new.xlsx'
+PATH_OLD = '.\\archive of notifications DSO\\notifications DSO 2022-07-29.xlsx'
+PATH_NEW = '.\\central archive OTD\\notifications DSO.xlsx'
+##PATH_OLD = '.\\archive of notifications DSO\\test_file_old.xlsx'
+##PATH_NEW = '.\\central archive OTD\\test_file_new.xlsx'
 PATH_DIFF = '.\\new receipts of DSO'
 
 
@@ -66,18 +66,88 @@ def frame_difference(frame_1: pd.DataFrame, frame_2: pd.DataFrame, keys: str):
 
 
 def write_frame_to_excel(frame: pd.DataFrame, name: str):
-    """Write frame to excel file."""
+    """Write data frame to excel file."""
     today = datetime.date.today()
-    writer = pd.ExcelWriter(name)
-    frame.to_excel(writer, sheet_name='New as of ' + str(today), index=False)
-    # Auto-adjust columns' width
+    first_sheet = 'New as of ' + str(today)
+    
+    writer = pd.ExcelWriter(name, datetime_format='dd.mm.yyyy')
+    frame.to_excel(writer, sheet_name=first_sheet, index=False)
+    workbook = writer.book
+    worksheet = writer.sheets[first_sheet]
+    
+    header_format = workbook.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'border': 1,
+        'valign': 'top',
+        'font': 'Times New Roman',
+        'size': '14'})
+    cell_format = workbook.add_format({
+        'align': 'center',
+        'valign': 'vcenter',
+        'font': 'Times New Roman',
+        'size': '14'})
+    
+    # Formatting header.
+    for col_num, value in enumerate(frame.columns.values):
+        worksheet.write(0, col_num, value, header_format)
+
+    # Cell merging.
+    columns = ['Город', 'Организация']
+    for column in columns:
+        identical_cells = find_identical_cells(frame, column)
+        for cells in identical_cells:
+            worksheet.merge_range(*cells, cell_format)
+
+    # Format column.
+    worksheet.set_column(0, 0, 30, cell_format)
+
+    # Auto-adjust columns' width.
     for column in frame:
         column_width = max(frame[column].astype(str).map(len).max(),
-                           len(column)) + 5
+                           len(column)) + 4
         col_idx = frame.columns.get_loc(column)
-        writer.sheets['New as of ' + str(today)].set_column(col_idx, col_idx,
-                                                            column_width)
+        worksheet.set_column(col_idx, col_idx, column_width)
+        
     writer.save()
+
+
+def find_identical_cells(df: pd.DataFrame, column: str):
+    """Find identical cells in a column in a data frame.
+    Return list of tuples of start cell, finish cell and value.
+    """
+    identical_cells = []
+    col_idx = df.columns.get_loc(column)
+    start = 0
+    finish = 0
+    value = None
+    first_match = True
+    print(1, df[column][0])
+    for i in range(1, len(df.index)):
+        print(i+1, df[column][i])
+        if df[column][i] == df[column][i-1] and first_match:
+            value = df[column][i-1]
+            start = i-1
+            finish = i
+            first_match = not first_match
+            continue
+        elif df[column][i] == df[column][i-1] and not first_match:
+            finish = i
+            continue
+        elif df[column][i] != df[column][i-1] and not first_match:
+            cells = (start+1, col_idx, finish+1, col_idx, value)
+            identical_cells.append(cells)
+            first_match = not first_match
+        elif df[column][i] != df[column][i-1] and first_match:
+            continue
+        
+    cells = (start+1, col_idx, finish+1, col_idx, value)
+    identical_cells.append(cells)
+            
+    for cells in identical_cells:
+        print(cells)
+
+    return identical_cells
 
 
 def main():
@@ -88,11 +158,18 @@ def main():
     data_new = pd.read_excel(PATH_NEW, skiprows=[0])
 
     data_diff = frame_difference(data_new, data_old, 'Обозначение документа')
+    
+    columns = ['Город', 'Организация', 'Обозначение документа',
+               'Наименование']
+    data_new = pd.DataFrame()
+    for column in columns:
+        data_new[column] = data_diff[column]
+##    print(data_new)
 
     # Write excel file.
     today = datetime.date.today()
-    name_diff = PATH_DIFF + '\\new receipts of DSO ' + str(today) + '.xlsx'   
-    write_frame_to_excel(data_diff, name_diff)
+    file_name = PATH_DIFF + '\\new receipts of DSO ' + str(today) + '.xlsx'   
+    write_frame_to_excel(data_new, file_name)
 
 
 
